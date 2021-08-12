25679046babe0273bac5943ef7e37190edd25f86
tomkoom <tomash.sugint@gmail.com>
2021-08-12 17:54:01 +0300

    write functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array) {
+    if (array.length > 0) {
+      return array.sort((a, b) => b - a).pop();
+    } else return 0;
+  } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array) {
+    if (array.length > 0) {
+      return array.sort((a, b) => a - b).pop();
+    } else return 0;
+  } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array) {
+    if (array.length > 0) {
+      return array.reduce((a, b) => a + b) / array.length;
+    } else return 0;
+  } else return 0;
 }
